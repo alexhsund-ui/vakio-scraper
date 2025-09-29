54b93aad31f99e4e92ac08e4687e87540283ea4c
Alexander Sund <alex.h.sund@gmail.com>
2025-09-29 14:04:59 +0300

    switch to Docker using Playwright base image

--- render-install.js
+++
@@ -1,3 +0,0 @@
-// Render kör "render-build" enligt package.json.
-// Vi kör installation via postinstall också, men detta script låter oss logga tydligare.
-console.log('Render install step: ensuring browsers are installed...');
